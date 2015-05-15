bb51281ae907bc053c3ca0a6994e1cec4d08b6b4
Jacy Wang <jacywj@gmail.com>
2015-05-15 11:36:07 -0700

    Finish V1

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -8,9 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate, UITextInputTraits>
 
 @property (strong, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (strong, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (strong, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+
+@property (assign, nonatomic) float billBeforeTips;
+@property (assign, nonatomic) float tips;
+@property (assign, nonatomic) float tipPercentage;
 
 @end
 
@@ -19,11 +25,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.billAmountTextField.delegate = self;
+    self.tipPercentageTextField.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)calculateTip:(UIButton *)sender {
+    [self.billAmountTextField resignFirstResponder];
+    [self.tipPercentageTextField resignFirstResponder];
+    NSLog(@"calculate bill - %f", self.billBeforeTips);
+    NSLog(@"calculate tipPecentage- %f", self.tipPercentage);
+    self.tips = self.billBeforeTips * self.tipPercentage;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"%.2f", self.tips];;
+    self.billAmountTextField.text = @"";
+    self.tipPercentageTextField.text = @"";
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+    [textField becomeFirstResponder];
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField == self.billAmountTextField) {
+        self.billBeforeTips = [textField.text floatValue];
+        NSLog(@"bill - %f", self.billBeforeTips);
+    } else if (textField == self.tipPercentageTextField) {
+        self.tipPercentage = [textField.text floatValue] / 100;
+         NSLog(@"tipPecentage - %f", self.tipPercentage);
+    }
 }
 
 @end
